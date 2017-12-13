a7a08b7cc815a2ad1c2e3c3a5cfeb778b88d8792
patchyj <jackjwmcgregor@gmail.com>
2017-12-13 21:36:18 +0000

    canvas stage 2 done

--- js/canvas.js
+++ js/canvas.js
@@ -6,6 +6,114 @@ canvas.height = window.innerHeight;
 // create the context in the form of the 'c', basically passing 'c' a ton of canvas methods and functions which we can use to draw with
 var c = canvas.getContext('2d');
 
+// var x = Math.random() * innerWidth;
+// var y = Math.random() * innerHeight;
+// var dx = (Math.random() - 0.5) * 10;
+// var dy = (Math.random() - 0.5) * 10;
+// var radius = 30
+
+function Circle(x, y, dx, dy, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.color = color;
+
+  this.randomNumber = function (num) {
+    return Math.floor(Math.random() * num);
+  }
+  this.randomColor = function () {
+    return "rgba(" + this.randomNumber(255) + "," + this.randomNumber(255) + "," + this.randomNumber(255) + ",.5)";
+  }
+
+  this.draw = function () {
+    c.beginPath();
+    // c.arc(x, y, radius, startAngle, endAngle, anticlockwise?)
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    // c.strokeStyle = 'blue';
+    // c.stroke();
+    c.fill();
+    c.fillStyle = this.color;
+    // use below for randomly generated colors every screen refresh
+    // c.fillStyle = this.randomColor();
+  }
+
+  this.update = function () {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx
+    }
+
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
+}
+
+
+
+var circleArray = [];
+
+for (let i = 0; i < 100; i++) {
+
+  var radius = Math.random() * 20;
+  var x = Math.random() * (innerWidth - (radius * 2)) + radius;
+  var y = Math.random() * (innerHeight - (radius * 2)) + radius;
+  var dx = (Math.random() - 0.5) * 10;
+  var dy = (Math.random() - 0.5) * 10;
+
+  randomNumber = function (num) {
+    return Math.floor(Math.random() * num);
+  }
+  randomColor = function () {
+    return "rgba(" + randomNumber(255) + "," + randomNumber(255) + "," + randomNumber(255) + ",.5)";
+  }
+
+  circleArray.push(new Circle(x, y, dx, dy, radius, randomColor()));
+
+}
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+
+  }
+
+  circle.update();
+
+
+
+}
+
+animate();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // // DRAWING RECTABGLES
 
 // // c.fillStyle = "#3c2" - change the fill color
@@ -49,51 +157,55 @@ var c = canvas.getContext('2d');
 
 // Comment In END
 
-// DRAWING MULTIPLE CIRCLES
-for (let i = 0; i < 10; i++) {
+// LOOP START ------------------------------------------
 
-  // To make a single shape appear multiple times across the screen you can the x and y coordinates as variables, like so:
+// // DRAWING MULTIPLE CIRCLES
+// for (let i = 0; i < 10; i++) {
 
-  // // set random variables so move the circles across the screen
-  // var x = Math.random() * window.innerWidth;
-  // var y = Math.random() * window.innerHeight;
+//   // To make a single shape appear multiple times across the screen you can the x and y coordinates as variables, like so:
 
-  // c.beginPath();
-  // c.arc(x,y,30,0,Math.PI * 2, false);
-  // c.strokeStyle = 'blue';
-  // c.stroke();
+//   // // set random variables so move the circles across the screen
+//   // var x = Math.random() * window.innerWidth;
+//   // var y = Math.random() * window.innerHeight;
 
-  // However, if we want to reuse those randomly generated coordinates for m ultiple shapes, we can make a callback function which generates a random number each time it is called, like so:
-  var randomNumber = function (num) {
-    return Math.floor(Math.random() * num);
-  }
+//   // c.beginPath();
+//   // c.arc(x,y,30,0,Math.PI * 2, false);
+//   // c.strokeStyle = 'blue';
+//   // c.stroke();
 
-  var randomColor = function () {
-    return "rgba(" + randomNumber(255) + "," + randomNumber(255) + "," + randomNumber(255) + ",.5)";
-  }
+//   // However, if we want to reuse those randomly generated coordinates for m ultiple shapes, we can make a callback function which generates a random number each time it is called, like so:
+//   var randomNumber = function (num) {
+//     return Math.floor(Math.random() * num);
+//   }
 
-  // the num is for the innerWidth/innerHeight, declared below
-  h = window.innerHeight;
-  w = window.innerWidth;
-  // For generating a random color
-  color = 255;
+//   var randomColor = function () {
+//     return "rgba(" + randomNumber(255) + "," + randomNumber(255) + "," + randomNumber(255) + ",.5)";
+//   }
 
-  // We can then pass in the window's width and height values to both the ARC and RECT pathmakers
-  c.beginPath();
-  c.arc(randomNumber(w), randomNumber(h), 30, 0, Math.PI * 2, false);
-  c.strokeStyle = randomColor();
-  c.stroke();
+//   // the num is for the innerWidth/innerHeight, declared below
+//   h = window.innerHeight;
+//   w = window.innerWidth;
+//   // For generating a random color
+//   color = 255;
 
-  // c.fillStyle = "#3c2" - change the fill color
-  c.fillStyle = randomColor();
-  // c.fillRect(x, y, width, height);
-  c.fillRect(randomNumber(w), randomNumber(h), 50, 50);
-  // canvas JS is cascading, meaning that properties can be changed if specified so further down the code
-  c.fillStyle = randomColor();
-  c.fillRect(randomNumber(w), randomNumber(h), 50, 50);
-  c.fillStyle = randomColor();
-  c.fillRect(randomNumber(w), randomNumber(h), 50, 50);
+//   // We can then pass in the window's width and height values to both the ARC and RECT pathmakers
+//   c.beginPath();
+//   c.arc(randomNumber(w), randomNumber(h), 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = randomColor();
+//   c.stroke();
+
+//   // c.fillStyle = "#3c2" - change the fill color
+//   c.fillStyle = randomColor();
+//   // c.fillRect(x, y, width, height);
+//   c.fillRect(randomNumber(w), randomNumber(h), 50, 50);
+//   // canvas JS is cascading, meaning that properties can be changed if specified so further down the code
+//   c.fillStyle = randomColor();
+//   c.fillRect(randomNumber(w), randomNumber(h), 50, 50);
+//   c.fillStyle = randomColor();
+//   c.fillRect(randomNumber(w), randomNumber(h), 50, 50);
 
 
 
-}
+// }
+
+// LOOP END ------------------------------------------
